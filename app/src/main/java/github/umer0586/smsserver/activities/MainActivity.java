b4fd0acfdb068370f815d0f231e5079df45b2b95
umer0586 <35717992+umer0586@users.noreply.github.com>
2022-10-11 21:43:12 +0500

    MainActivity.java Updated

--- app/src/main/java/github/umer0586/smsserver/activities/MainActivity.java
+++ app/src/main/java/github/umer0586/smsserver/activities/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_2);
+        setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.viewpager);
         tabLayout = findViewById(R.id.tab_layout);
